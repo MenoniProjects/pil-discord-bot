614f3e541ada60d973f9395a2de1e6f1ca1882a8
Wouter Gerarts <wouto1997@gmail.com>
2025-02-15 12:15:51 +0100

    allow using role ids in place of `@role` arguments

--- src/main/java/net/menoni/pil/bot/util/DiscordArgUtil.java
+++ src/main/java/net/menoni/pil/bot/util/DiscordArgUtil.java
@@ -6,11 +6,23 @@ public class DiscordArgUtil {
 
 	private static final char ZERO = '0';
 	private static final char NINE = '9';
+	private static final Pattern ID_PATTERN = Pattern.compile("([0-9]+)");
 	private static final Pattern ROLE_PATTERN = Pattern.compile("<@&([0-9]+)>");
 	private static final Pattern EMOTE_PATTERN = Pattern.compile("<:([a-zA-Z0-9_-]+):([0-9]+)>");
 
+	public static boolean isNudeId(String arg) {
+		return ID_PATTERN.matcher(arg).matches();
+	}
+
+	public static String getNudeId(String arg) {
+		if (!isNudeId(arg)) {
+			return null;
+		}
+		return getNumericCharacters(arg);
+	}
+
 	public static boolean isRole(String arg) {
-		return ROLE_PATTERN.matcher(arg).matches();
+		return ID_PATTERN.matcher(arg).matches() || ROLE_PATTERN.matcher(arg).matches();
 	}
 
 	public static String getRoleId(String arg) {
