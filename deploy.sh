aaa096bf84be74573d3731203d22afaef8f93b3a
Wouter Gerarts <wouto1997@gmail.com>
2025-05-14 03:21:12 +0200

    Change repository user->org references for migration

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --build-arg JAR_FILE=target/pil-discord-bot.jar -t ghcr.io/wouterg/pil-discord:dev .
-docker push ghcr.io/wouterg/pil-discord:dev
+docker build --build-arg JAR_FILE=target/pil-discord-bot.jar -t ghcr.io/menoniprojects/pil-discord:dev .
+docker push ghcr.io/menoniprojects/pil-discord:dev
